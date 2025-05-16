ff39741c70c72f89d9615216f02d04991851fe6e
OutKast317 <phonewinttun173@gmail.com>
2025-05-16 21:20:56 +0630

    feat: python programs

---
+++ discount.py
@@ -0,0 +1,3 @@
+
+amount = float(input("Enter total amount: "))
+print(f"Total amount is {amount} \n You need to pay {amount - (amount * 0.1)}")
---
+++ test.py
@@ -0,0 +1,44 @@
+'''
+
+width = input("Width: ")
+length = input("Length: ")
+int_width = int(width)
+int_length = int(length)
+area = int_length * int_width
+print("Area: ", area)
+
+'''
+
+'''
+
+name = input("Enter name: ")
+age = input("Enter date of birth: ")
+int_age = int(age)
+print("Your name is", name + age)
+print("Your age is",2025 - int_age)
+
+'''
+
+'''
+
+name = input("Enter name: ")
+date_of_birth = input("Enter date of birth: ")
+occupation = input("Enter occupation: ")
+print("Your name:", name , "\n" , "Age:", 2025 - int(date_of_birth), "\n", "Occupation:", occupation)
+
+'''
+
+name = input("Hello, what's your name? \n")
+print(f"Hello,{name}!")
+date_of_birth = int(input("Please tell us your date of birth: "))
+address = input("Where do you live? \n")
+occupation = input("What is your occupation? \n")
+hobby = input("What is your hobby? \n")
+
+print("\nPersonal information")
+print("______________________")
+print(f"Name: {name}")
+print(f"Age: {2025 - date_of_birth}")
+print(f"Address: {address}")
+print(f"Occupation: {occupation}")
+print(f"Hobby: {hobby}")
