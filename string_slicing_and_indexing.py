4000fd4418a70f121a5c5f4316861052510dd4e0
OutKast317 <phonewinttun173@gmail.com>
2025-05-18 23:51:06 +0630

    feat: copying a string

--- string_slicing_and_indexing.py
+++ string_slicing_and_indexing.py
@@ -10,3 +10,7 @@ example_string = "Terracotta pie, banana"
 print(example_string[:11])
 print(example_string[11:14])
 print(example_string[16:])  
+
+#copying a string
+copy_string = example_string[::-1]  # reverse the string
+print(copy_string)
