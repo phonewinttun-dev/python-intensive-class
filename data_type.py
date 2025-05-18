3d48842a0a61d4e05766496260813920b6cb4e13
OutKast317 <phonewinttun173@gmail.com>
2025-05-18 20:12:41 +0630

    feat: data type

---
+++ data_type.py
@@ -0,0 +1,21 @@
+variable = "Hello, World!"
+print(variable)
+variable = 5
+print(variable)
+#dynamic data type
+
+name = "George"
+type_name = type(name)
+print(type_name)
+
+number = 10
+type_number = type(number)
+print(type_number)
+
+number2 = 17.3
+type_number2 = type(number2)
+print(type_number2)
+
+is_valid = True
+type_is_valid = type(is_valid)
+print(type_is_valid)
--- temperature_converter.py
+++ temperature_converter.py
@@ -1,7 +1,14 @@
+#celsius to fahrenheit equation = (C * 9/5) + 32
+
 temp_celsius_input = float(input("Enter temperature in Celsius: "))
 temp_fahrenheit = (temp_celsius_input * 9/5) + 32
 print(f"Temperature in Fahrenheit: {temp_fahrenheit} °F")
 
+# fahrenheit to celsius equation = (F - 32) * 5/9
+
 temp_fahrenheit_input = float(input("Enter temperature in Fahrenheit: "))
 temp_celsius = (temp_fahrenheit_input - 32) * 5/9
 print(f"Temperature in Celsius: {temp_celsius} °C")
+
+
+#Alt + 0176
