ac4afe590fbd2898ef31f182d21a4072b5c99ccd
OutKast317 <phonewinttun173@gmail.com>
2025-05-18 20:32:16 +0630

    feat: string data type

---
+++ string_data_type.py
@@ -0,0 +1,19 @@
+#string data type 
+# single line support -> ' ', " "
+# multi line support -> ''' ''', """ """
+# string -> collection of characters
+
+name = 'Phone Wint Tun'
+print(name)
+
+length_of_name = len(name)
+print(length_of_name)
+
+# string indexing
+name_index = name[0]
+print(name_index)
+
+print("\nMulti line string:")
+multi_line_string = """Hello, my name is Phone Wint Tun.
+I am 20 years old."""
+print(multi_line_string)
