f955a504b2b38fa5a3b995a503c989425f1d8249
OutKast317 <phonewinttun173@gmail.com>
2025-05-18 23:46:47 +0630

    feat: string slicing and indexing

---
+++ string_slicing_and_indexing.py
@@ -0,0 +1,12 @@
+
+string_variable = "Hello, my name is Phone Wint Tun."
+print(string_variable[0]) # string indexing
+print(string_variable[-2]) # string reverse indexing
+print(string_variable[0:5])  # string slicing  
+print(string_variable[0:10:2])  # string slicing with step (start : end : step)
+
+# extracting substring from a string
+example_string = "Terracotta pie, banana"
+print(example_string[:11])
+print(example_string[11:14])
+print(example_string[16:])  
