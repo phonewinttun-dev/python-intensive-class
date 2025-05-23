395017fb0b5bde5985e8f6d5f0cb01436f81c362
OutKast317 <phonewinttun173@gmail.com>
2025-05-23 20:38:59 +0630

    feat: password strength checker

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -1,8 +1,38 @@
-while True:
-  password_input = input("Enter a password: ")
+def password_strength_checker():
+
+  while True:
+    password_input = input("Enter a password: ")
   
-  if len(password_input) >= 6 and password_input.isalnum():
-    print("Strong password.")
-    break
-  else:
-    print("Password must be at least six characters.")
+    if ' ' in password_input:
+      print("Password cannot contain spaces.")
+      continue
+
+    if len(password_input) < 8:
+      print("Password must be at least 8 characters.")
+      continue
+
+    has_uppercase = False
+    has_lowercase = False
+    has_digit = False
+
+    for password in password_input:
+      if password.isupper():
+        has_uppercase = True
+      elif password.islower():
+        has_lowercase = True
+      elif password.isdigit():
+        has_digit = True
+
+    if has_digit and has_lowercase and has_uppercase:
+      print("Your password is strong.")
+      break
+
+    else:
+      if not has_digit:
+        print("You need at least one digit.")
+      if not has_lowercase:
+        print("You need at least one lowercase character.")
+      if not has_uppercase:
+        print("You need at least one uppercase character.")
+
+password_strength_checker()
