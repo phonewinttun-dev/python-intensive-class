0bf781dddda9f896fcca240d24b47547f5460f99
OutKast317 <phonewinttun173@gmail.com>
2025-05-24 04:31:57 +0630

    multiplication table

--- factorial_calculator.py
+++ factorial_calculator.py
@@ -4,3 +4,14 @@ for i in range(1, num + 1):
   result *= i
   
 print(f"Factorial of {num} is {result}")
+
+'''
+Visualization of the calculation
+
+result = 1 * 1 => 1
+result = 1 * 2 => 2
+result = 2 * 3 => 6
+result = 6 * 4 => 24
+result = 24 * 5 => 120
+
+'''
