1b23c8c99d05c0a1125180663969716ae2146843
@OutKast317 <phonewinttun173@gmail.com>
2025-05-31 22:03:21 +0630

    library database updated

--- Library Management System/library_database.py
+++ Library Management System/library_database.py
@@ -30,3 +30,25 @@ books = [
    }
 ]
 
+book_collection = {} #dictionary to store Book objects
+
+for b in books:
+  book_id = b["ID"]
+  title = b["Title"]
+  author = b["Author"]
+  genre = b["Genre"]
+  is_available = b["Is_Available"]
+
+  new_book_added = Book(book_id, title, author, genre, is_available)
+
+  book_collection[book_id] = new_book_added
+
+'''
+Checking if correct
+print(f"Total books in collection: {len(book_collection)}")
+
+book_one = book_collection[1]
+print(f"Accessed book ID 1: {book_one.title}")
+print(book_one.display_info())
+'''
+
