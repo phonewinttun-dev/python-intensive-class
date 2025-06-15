c972496dcd50ef37d8ae339fffb0c854f6018ec3
@OutKast317 <phonewinttun173@gmail.com>
2025-06-16 01:21:25 +0630

    feat: can update student's info now

--- Student Management System/student_main.py
+++ Student Management System/student_main.py
@@ -33,12 +33,34 @@ def main():
       studmgmt.enroll_student(new_student)
       save_data(studmgmt.students, "students_data.txt")
       print("-----Enrollment Successful!-----")
-      
+
     elif option == 2:
       studmgmt.view_student()
         
     elif option == 3:
-      pass
+      student_id = input("Enter the ID of the student you want to update:")
+      student_name = input("Enter new student name(press Enter to skip): ")
+      while True:
+        student_age_input = input("Enter new student age(press Enter to skip): ")
+        if student_age_input.strip() == "":
+          student_age = ""
+          break
+        if not student_age_input.isdigit():
+            print("Please enter a nnumber.")
+            continue
+        student_age = int(student_age_input)
+        if (0 < student_age >= 100): 
+            print("Please enter a valid age.")
+        else:
+          break
+      major = input("Enter new student major(press Enter to skip): ")
+      updated_student = studmgmt.update_info(student_id, student_name, student_age, major)
+      if updated_student:
+        print("-----Student Info updated successfully!-----")
+        save_data(studmgmt.students, "students_data.txt")
+      else:
+        print("Student not found! Please enter the ID of existing student!")
+
     
     elif option == 4:
       pass
