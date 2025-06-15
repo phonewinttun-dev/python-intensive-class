4bc11b734e68c5c0f92f306c196e9e5492723541
@OutKast317 <phonewinttun173@gmail.com>
2025-06-16 00:23:52 +0630

    studmgmtsys.py module's functions modified to compatible with file system

--- Student Management System/studmgmtsys.py
+++ Student Management System/studmgmtsys.py
@@ -1,9 +1,8 @@
-from db import students
 from student_class import Students
-from utilities import search_item
+
 
 class StudentManagementSystem:
-  def __init__(self):
+  def __init__(self, students):
     self.students = students
   
   def id_auto_generator(self):
@@ -25,28 +24,9 @@ class StudentManagementSystem:
     print("5. Exit")
     print("======================")
 
-  def enroll_student(self):
-    student_id = self.id_auto_generator()
-      
-    name = input("Enter a student name: ")
-    while True:
-      age = input("Enter age: ")
-
-      if not age.isdigit():
-        print("Please enter a digit for age.")
-      else:
-        int_age = int(age)
-
-        if int_age <= 0:
-          print("Error! Please enter a valid age.")
-        else:
-          break
-        
-    major = input("Enter a specialized major: ")
-    new_student = Students(student_id, name, int_age, major)
-    
-    self.students.append(new_student)
-    print("Enrollment successful!")
+  def enroll_student(self, student_info):
+    self.students.append(student_info)
+    return True
 
   def view_student(self):
       print("======================")
@@ -55,33 +35,21 @@ class StudentManagementSystem:
       for stud in self.students:
         print(f"ID: {stud.id} Name: {stud.name}\t Age: {stud.age}\t Major: {stud.major}")
 
-  def update_info(self):
-    student_id = input("Enter ID of the student you want to update: ")
-    wanted_student = None
-    wanted_student = search_item(student_id, self.students)
-    if wanted_student:
-      print(f"Current info of the student: {wanted_student.display_info()}")
-      name = input(f"Enter new name(leave blank to keep current): ")
-      age = input("Enter age(leave blank to keep current): ")
-      int_age = int(age)
-      major = input(f"Enter new major(leave blank to keep current): ")
-    
-      if name:
-        wanted_student.name = name
-    
-      if age:
-        wanted_student.age = int_age
-
-      if major:
-        wanted_student.major = major
-
-    print(f"{wanted_student.name} is updated successfully!")
-
-  def remove_student(self):
-    student_id = input("Enter ID of the student you want to remove: ")
-    wanted_student = search_item(student_id, self.students)
-    if wanted_student in self.students:
-      self.students.remove(wanted_student)
-      print(f"{wanted_student.name} is removed successfully!")
-    else:
-      print("Student does not exist.")
+  def update_info(self, student_id, student_name, student_age, major):
+    for stud in self.students:
+      if stud.id == student_id:
+        if student_name:
+          stud.name = student_name
+        if student_age:
+          stud.age = student_age
+        if major:
+          stud.major = major
+        return True
+    return False
+      
+  def remove_student(self, student_id):
+    for stud in self.students:
+      if stud.id == student_id:
+        self.students.remove(stud)
+        return True
+    return False
